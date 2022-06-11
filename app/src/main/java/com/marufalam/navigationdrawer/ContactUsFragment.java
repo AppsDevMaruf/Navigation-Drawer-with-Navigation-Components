ff237b8755e1057603a2bcef51a11f32d7f5c8d5
maruf-alam <marufalam120@gmail.com>
2022-06-11 10:28:26 +0600

    add media

--- app/src/main/java/com/marufalam/navigationdrawer/ContactUsFragment.java
+++ app/src/main/java/com/marufalam/navigationdrawer/ContactUsFragment.java
@@ -1,64 +1,70 @@
 package com.marufalam.navigationdrawer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContactUsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ContactUsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button play, pause, stop;
+    MediaPlayer player;
 
     public ContactUsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContactUsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContactUsFragment newInstance(String param1, String param2) {
-        ContactUsFragment fragment = new ContactUsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        play = view.findViewById(R.id.button1);
+        pause = view.findViewById(R.id.button2);
+        stop = view.findViewById(R.id.button3);
+
+        play.setOnClickListener(view1 -> {
+
+            if (player==null){
+                player = MediaPlayer.create(getActivity(),R.raw.music);
+                player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        stopPlayer();
+                    }
+                });
+            }
+            player.start();
+        });
+        pause.setOnClickListener(view1 -> {
+            if (player!=null){
+                player.pause();
+            }
+        });
+        stop.setOnClickListener(view1 -> stopPlayer());
+
+
+        return view;
+
+    }
+
+    private void stopPlayer(){
+        if (player!=null){
+            player.release();
+            player=null;
+            Toast.makeText(requireActivity(), "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        stopPlayer();
     }
 }
